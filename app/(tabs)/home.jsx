2460dc4cf2fe87a6ca7b1e56dacb20dad32c597c
Nadil Senruka <it22144430@my.sliit.lk>
2024-08-07 23:25:55 +0530

    Home page
    hard code home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,37 +1,57 @@
-import { View, Image, Text, ScrollView, TouchableOpacity } from 'react-native';
-import React from 'react';
-import { icons } from '../../constants';
-import { useRouter } from 'expo-router';
+import {
+  View,
+  Image,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import React from "react";
+import { icons } from "../../constants";
+import { useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useGlobalContext } from '../../context/GlobalProvider';
-
+import { useGlobalContext } from "../../context/GlobalProvider";
+import SearchInput from "../../components/SearchInput";
 
 const Home = () => {
-  const router = useRouter(); 
+  const router = useRouter();
 
-  
   const handleProfileNavigation = () => {
-    router.push('../profile'); 
+    router.push("../profile");
   };
 
   return (
-    <SafeAreaView>
-      <ScrollView contentContainerStyle={{height:'100%'}} >
-    <View className="w-full justify-center items-center mt-6 mb-12 px-4 ">
-    <TouchableOpacity
-      onPress={handleProfileNavigation}
-      className="bg-primary rounded-full p-2 absolute right-4 top-1"
-    >
-        <Image
-          source={icons.profile}
-          resizeMode="contain"
-          style={{ width: 24, height: 24, tintColor: '#FFA001', }} 
-        />
-      </TouchableOpacity>
-
-     
-    </View>
-    </ScrollView>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ id: 1 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <Text className="text-3xl">{item.id}</Text>}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <TouchableOpacity
+              onPress={handleProfileNavigation}
+              className="bg-primary rounded-full p-2 absolute right-4 top-1"
+            >
+              <Image
+                source={icons.profile}
+                resizeMode="contain"
+                style={{ width: 24, height: 24, tintColor: "#FFA001" }}
+              />
+            </TouchableOpacity>
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  Nadil Liyanage
+                </Text>
+              </View>
+            </View>
+            <SearchInput />
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
