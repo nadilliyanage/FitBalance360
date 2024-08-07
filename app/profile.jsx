c7a3c9182e89d6a3eb15412d4e8f5337ff00da8c
Nadil Senruka <it22144430@my.sliit.lk>
2024-08-07 18:23:12 +0530

    edit

--- app/profile.jsx
+++ app/profile.jsx
@@ -21,7 +21,7 @@ const Profile = () => {
 
   return (
     <View className="w-full justify-center items-center mt-6 mb-12 px-4">
-      <TouchableOpacity className="w-full items-end mb-10" onPress={logout}>
+      <TouchableOpacity className="w-full items-end mb-10 mt-10" onPress={logout}>
         <Image source={icons.logout} resizeMode='contain' className="w-6 h-6" />
       </TouchableOpacity>
     </View>
